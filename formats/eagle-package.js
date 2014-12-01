1ffc2634aa6c4e9e0a43037c83d4b303cecf0d13
Matthias <mail@matthiasbock.net>
2014-12-01 21:31:56 +0100

    Eagle package import works pretty well

--- formats/eagle-package.js
+++ formats/eagle-package.js
@@ -11,20 +11,47 @@ EaglePackage = function(name)
 {
     this.name = name ? name : '';
     this.description = '';
-    this.components = {
-        'polygon':   [],
-        'wire':      [],
-        'text':      [],
-        'dimension': [],
-        'circle':    [],
-        'rectangle': [],
-        'frame':     [],
-        'hole':      [],
-        'pad':       [],
-        'smd':       []
-        };
+
+    this.elements = {};
+    for (key in EaglePackageElements)
+    {
+        // empty list of respective component
+        this.elements[key] = [];
+    } 
 }
 
+/*
+ * Dictionary of valid package sub-elements
+ * with valid sub-element attributes
+ */
+EaglePackageElements ={
+    'polygon':   [],
+
+    // <wire x1="-1.4" y1="-0.8" x2="-1.4" y2="0.8" width="0.127" layer="51"/>
+    'wire':      ['x1','y1','x2','y2','width','layer'],
+
+    // <text x="-5.08" y="-2.54" size="1.27" layer="25" rot="R90">&gt;NAME</text>
+    'text':      ['x','y','size','layer','rot'],
+
+    'dimension': [],
+    
+    // <circle x="3.59" y="-0.7699" radius="0.4999" width="0.1016" layer="51"/>
+    'circle':    ['x','y','radius','width','layer'],
+
+    // <rectangle x1="1" y1="47" x2="13" y2="52" layer="1"/>
+    'rectangle': ['x1','y1','x2','y2','layer'],
+
+    'frame':     [],
+    'hole':      [],
+
+    // <pad name="4" x="1.25" y="2.71" drill="0.95" shape="octagon"/>
+    'pad':       ['name','x','y','drill','shape'],
+
+    // <smd name="16" x="-4.445" y="2.695" dx="0.635" dy="1.524" layer="1"/>
+    'smd':       ['name','x','y','dx','dy','layer'],
+};
+
+// recognized via shapes
 EagleViaShape = [
     'round'
 ];
@@ -68,40 +95,40 @@ EaglePackage.prototype.importjQuery = function(jElement)
         console.error('Error: '+e.length+' <description/> elements. Must be exactly one.');
     }
 
-    // Which elements shall we parse and how?
-    var parserFunctions = {
-        // <wire x1="-1.4" y1="-0.8" x2="-1.4" y2="0.8" width="0.127" layer="51"/>
-        'wire': function(jElement)
-                {
-                    return jParse(jElement, ['x1','y1','x2','y2','width','layer']);
-                },
-        // <pad name="4" x="1.25" y="2.71" drill="0.95" shape="octagon"/>
-        'pad':  function(jElement)
-                {
-                    return jParse(jElement, ['name','x','y','drill','shape']);
-                },
-        // <smd name="16" x="-4.445" y="2.695" dx="0.635" dy="1.524" layer="1"/>
-        'smd':  function(jElement)
-                {
-                    return jParse(jElement, ['name','x','y','dx','dy','layer']);
-                },
-        // <text x="-5.08" y="-2.54" size="1.27" layer="25" rot="R90">&gt;NAME</text>
-        'text': function(jElement)
-                {
-                    return jParse(jElement, ['x','y','size','layer','rot']);
-                },
-        };
+    // parser, that uses above EaglePackageElements to parse all valid element attributes
+    parser = function(name, element)
+    {
+        element = $(element);
+        var result = jParse(element, EaglePackageElements[name]);
+        // in case of text, also parse the text
+        if (name == 'text')
+            result['text'] = element.html();
+        // in case of polygon, also parse vertex sub-elements
+        else if (name == 'polygon')
+        {
+            result.vertices = [];
+            element.children('vertex').each(function()
+            {
+                var self = $(this);
+                result.vertices.push({
+                    'x': self.attr('x'),
+                    'y': self.attr('y')
+                    });
+            });
+        }
+        return result;
+    } 
 
     // Parse one by one ...
-    for (key in parserFunctions)
+    for (key in EaglePackageElements)
     {
-        // Find all of this type
+        // Find all of this element type
         var e = jElement.find(key);
         for (var i=0; i<e.length; i++)
         {
-            // invoke specified parser onto element
+            // invoke parser onto element
             // append result to array
-            this.components[key].push( parserFunctions[key]($(e[i])) );
+            this.elements[key].push( parser(key, e[i]) );
         }
         console.log(e.length+' elements of type <'+key+'/>');
     }
@@ -114,7 +141,7 @@ EaglePackage.prototype.importjQuery = function(jElement)
  */
 EaglePackage.prototype.addHole = function(x, y, drill)
 {
-    this.components['hole'].push(
+    this.elements['hole'].push(
     {
         'x':     x,
         'y':     y,
@@ -124,7 +151,7 @@ EaglePackage.prototype.addHole = function(x, y, drill)
 
 EaglePackage.prototype.addPad = function(name, x, y, drill, shape)
 {
-    this.components['pad'].push(
+    this.elements['pad'].push(
     {
         'name':  name,
         'x':     x,
@@ -143,11 +170,11 @@ EaglePackage.prototype.exportjQuery = function()
     if (this.name == '')
         console.error('Warning: Package has no name');
     
-    for (key in this.components)
+    for (key in this.elements)
     {
-        for (var i=0; i<this.components[key].length; i++)
+        for (var i=0; i<this.elements[key].length; i++)
         {
-            result.append(json2jquery(key, this.components[key][i]));
+            result.append(json2jquery(key, this.elements[key][i]));
         }
     }
 
@@ -172,22 +199,29 @@ EaglePackage.prototype.exportString = function(noSelfClosing)
  */
 EagleRenderSVG = {
     'polygon':  function(polygon) {
-                    return '';
+                    var result = '<path id="'+polygon.name+'" d="';
+                    for (var i=0; i<polygon.vertices.length; i++)
+                        result += (i==0 ? 'M' : 'L')+EagleRenderSVG['vertex'](polygon.vertices[i]);
+                    result += 'Z"/>';
+                    return result;
+                },
+    'vertex':   function(vertex) {
+                    return vertex.x+','+vertex.y;
                 },
     'wire':     function(wire) {
-                    return '';
+                    return '<line class="wire" layer="'+EagleLayers[wire.layer]+'" x1="'+wire.x1+'" y1="'+wire.y1+'" x2="'+wire.x2+'" y2="'+wire.y2+'" stroke-width="'+wire.width+'"/>';
                 },
     'text':     function(text) {
-                    return '';
+                    return '<text layer="'+EagleLayers[text.layer]+'" x="'+text.x+'" y="'+text.y+'" font-size="'+text.size+'">'+text.text+'</text>';
                 },
     'dimension':function(dimension) {
                     return '';
                 },
     'circle':   function(circle) {
-                    return '';
+                    return '<circle class="circle" layer="'+EagleLayers[circle.layer]+'" cx="'+circle.x+'" cy="'+circle.y+'" r="'+(parseFloat(circle.radius)+parseFloat(circle.width))+'" />';
                 },
     'rectangle':function(rectangle) {
-                    return '';
+                    return '<rect class="rectangle" layer="'+EagleLayers[rectangle.layer]+'" x="'+rectangle.x1+'" y="'+rectangle.y1+'" width="'+(rectangle.x2-rectangle.x1)+'" height="'+(rectangle.y2-rectangle.y1)+'"/>';
                 },
     'frame':    function(frame) {
                     return '';
@@ -199,7 +233,7 @@ EagleRenderSVG = {
                     return '';
                 },
     'smd':      function(smd) {
-                    return '<rect class="smd" id="smd'+smd.name+'" x="'+(smd.x-(smd.dx/2))+'" y="'+(smd.y-(smd.dy/2))+'" width="'+smd.dx+'" height="'+smd.dy+'" layer="'+smd.layer+'"/>';
+                    return '<rect class="smd" id="'+smd.name+'" layer="'+EagleLayers[smd.layer]+'" x="'+(smd.x-(smd.dx/2))+'" y="'+(smd.y-(smd.dy/2))+'" width="'+smd.dx+'" height="'+smd.dy+'"/>';
                 }
 };
 
@@ -211,11 +245,11 @@ EaglePackage.prototype.exportSVG = function()
     var svg = '<svg width="200" height="200">';
     svg += '<g transform="translate(100,100) scale(10)">';
 
-    for (key in this.components)
+    for (key in this.elements)
     {
-        for (var i=0; i<this.components[key].length; i++)
+        for (var i=0; i<this.elements[key].length; i++)
         {
-            svg += EagleRenderSVG[key]( this.components[key][i] );
+            svg += EagleRenderSVG[key]( this.elements[key][i] );
         }
     }
 
