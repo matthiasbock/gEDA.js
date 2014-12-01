445de40ee2aadf7ea1be0d940a88af4f11ac03be
Matthias <mail@matthiasbock.net>
2014-12-01 20:27:05 +0100

    basic Eagle package to SVG works

--- formats/eagle-package.js
+++ formats/eagle-package.js
@@ -1,31 +1,33 @@
 
 /*
  * JavaScript object for Eagle PCB packages (*.pac or several in one *.lbr)
+ *
+ * <!ELEMENT package (description?, (polygon | wire | text | dimension | circle | rectangle | frame | hole | pad | smd)*)>
+ * <!ATTLIST package
+ *         name          %String;       #REQUIRED
+ *         >
  */
-EaglePackage = function()
+EaglePackage = function(name)
 {
-    this.name = '';
+    this.name = name ? name : '';
     this.description = '';
-    this.pads = [];
-    this.holes = [];
-    this.texts = [];
+    this.components = {
+        'polygon':   [],
+        'wire':      [],
+        'text':      [],
+        'dimension': [],
+        'circle':    [],
+        'rectangle': [],
+        'frame':     [],
+        'hole':      [],
+        'pad':       [],
+        'smd':       []
+        };
 }
 
 EagleViaShape = [
     'round'
 ];
-
-// <smd name="16" x="-4.445" y="2.695" dx="0.635" dy="1.524" layer="1"/>
-parseEaglePad = function(jElement)
-{
-    return jParse(jElement, ['name','x','y','dx','dy','layer']);
-}
-
-// <text x="-5.08" y="-2.54" size="1.27" layer="25" rot="R90">&gt;NAME</text>
-parseEagleText = function(jElement)
-{
-    return jParse(jElement, ['x','y','size','layer','rot']);
-}
 
 /*
  * Parse Eagle package from XML using jQuery:
@@ -34,11 +36,29 @@ parseEagleText = function(jElement)
  * ...
  * </package>
  */
-EaglePackage.prototype.fromDOM = function(jElement)
+EaglePackage.prototype.importjQuery = function(jElement)
 {
+    console.log('Importing package: "'+this.name+'"');
+
+    // <package name="...">
     this.name = jElement.attr('name');
-    console.log('Importing symbol: '+this.name);
-        
+    
+    /*
+     * Browser problem:
+     * Eagle XML is not valid HTML. Browsers fail trying to understand the
+     * self-closing tags that most of the Eagle elements contain.
+     * Accordingly, elements following self-closing elements are falsely
+     * interpreted as sub-elements.
+     * Fix:
+     * Find all sub-elements to element and move them after element
+     */
+    jElement.find('wire,text,dimension,circle,rectangle,frame,hole,pad,smd,vertex').each(function()
+    {
+        var self = $(this);
+        self.children().insertAfter(self);
+    });
+
+    // <description>...</description>
     var e = jElement.find('description');
     if (e.length == 1)
     {
@@ -48,30 +68,45 @@ EaglePackage.prototype.fromDOM = function(jElement)
         console.error('Error: '+e.length+' <description/> elements. Must be exactly one.');
     }
 
-    // Which elements shall we parse?
-    var parseMe = {
-        'smd': [
-            parseEaglePad,
-            this.pads,
-            ],
-        'text': [
-            parseEagleText,
-            this.texts
-            ]
-    }
+    // Which elements shall we parse and how?
+    var parserFunctions = {
+        // <wire x1="-1.4" y1="-0.8" x2="-1.4" y2="0.8" width="0.127" layer="51"/>
+        'wire': function(jElement)
+                {
+                    return jParse(jElement, ['x1','y1','x2','y2','width','layer']);
+                },
+        // <pad name="4" x="1.25" y="2.71" drill="0.95" shape="octagon"/>
+        'pad':  function(jElement)
+                {
+                    return jParse(jElement, ['name','x','y','drill','shape']);
+                },
+        // <smd name="16" x="-4.445" y="2.695" dx="0.635" dy="1.524" layer="1"/>
+        'smd':  function(jElement)
+                {
+                    return jParse(jElement, ['name','x','y','dx','dy','layer']);
+                },
+        // <text x="-5.08" y="-2.54" size="1.27" layer="25" rot="R90">&gt;NAME</text>
+        'text': function(jElement)
+                {
+                    return jParse(jElement, ['x','y','size','layer','rot']);
+                },
+        };
+
     // Parse one by one ...
-    for (key in parseMe)
+    for (key in parserFunctions)
     {
         // Find all of this type
         var e = jElement.find(key);
-        console.log('Found '+e.length+' elements of type <'+key+'>.');
         for (var i=0; i<e.length; i++)
         {
             // invoke specified parser onto element
             // append result to array
-            parseMe[key][1].push( parseMe[key][0](e) );
+            this.components[key].push( parserFunctions[key]($(e[i])) );
         }
+        console.log(e.length+' elements of type <'+key+'/>');
     }
+    
+    return this;
 }
 
 /*
@@ -79,10 +114,23 @@ EaglePackage.prototype.fromDOM = function(jElement)
  */
 EaglePackage.prototype.addHole = function(x, y, drill)
 {
-    this.holes.push({
+    this.components['hole'].push(
+    {
         'x':     x,
         'y':     y,
         'drill': drill
+    });
+}
+
+EaglePackage.prototype.addPad = function(name, x, y, drill, shape)
+{
+    this.components['pad'].push(
+    {
+        'name':  name,
+        'x':     x,
+        'y':     y,
+        'drill': drill,
+        'shape': shape
     });
 }
 
@@ -95,14 +143,11 @@ EaglePackage.prototype.exportjQuery = function()
     if (this.name == '')
         console.error('Warning: Package has no name');
     
-    var exportElements = {
-        'hole': this.holes
-        };
-    for (key in exportElements)
+    for (key in this.components)
     {
-        for (var i=0; i<exportElements[key].length; i++)
+        for (var i=0; i<this.components[key].length; i++)
         {
-            result.append(json2jquery(key, exportElements[key][i]));
+            result.append(json2jquery(key, this.components[key][i]));
         }
     }
 
@@ -112,10 +157,68 @@ EaglePackage.prototype.exportjQuery = function()
 /*
  * Export package as string
  */
-EaglePackage.prototype.exportString = function()
+EaglePackage.prototype.exportString = function(noSelfClosing)
 {
-    return this.exportjQuery()
-            .prop('outerHTML')
-            .replace(/></g,'>\n<')
-            .replace(/(\<[a-z]* [^>]*)\>[\s]*\<\/[a-z]*\>/g, '$1/>');
+    var result = this.exportjQuery()
+        .prop('outerHTML')
+        .replace(/></g,'>\n<');
+    if (!noSelfClosing)
+        result = result.replace(/(\<[a-z]* [^>]*)\>[\s]*\<\/[a-z]*\>/g, '$1/>');
+    return result;
+}
+
+/*
+ * How to draw SVG elements for each Eagle package sub-element
+ */
+EagleRenderSVG = {
+    'polygon':  function(polygon) {
+                    return '';
+                },
+    'wire':     function(wire) {
+                    return '';
+                },
+    'text':     function(text) {
+                    return '';
+                },
+    'dimension':function(dimension) {
+                    return '';
+                },
+    'circle':   function(circle) {
+                    return '';
+                },
+    'rectangle':function(rectangle) {
+                    return '';
+                },
+    'frame':    function(frame) {
+                    return '';
+                },
+    'hole':     function(hole) {
+                    return '<circle class="hole" cx="'+hole.x+'px" cy="'+hole.y+'px" r="'+(hole.drill/2)+'px"/>';
+                },
+    'pad':      function(pad) {
+                    return '';
+                },
+    'smd':      function(smd) {
+                    return '<rect class="smd" id="smd'+smd.name+'" x="'+(smd.x-(smd.dx/2))+'" y="'+(smd.y-(smd.dy/2))+'" width="'+smd.dx+'" height="'+smd.dy+'" layer="'+smd.layer+'"/>';
+                }
+};
+
+/*
+ * Export package as Scalable Vector Graphic using jQuery
+ */
+EaglePackage.prototype.exportSVG = function()
+{
+    var svg = '<svg width="200" height="200">';
+    svg += '<g transform="translate(100,100) scale(10)">';
+
+    for (key in this.components)
+    {
+        for (var i=0; i<this.components[key].length; i++)
+        {
+            svg += EagleRenderSVG[key]( this.components[key][i] );
+        }
+    }
+
+    svg += '</g></svg>';
+    return svg;
 }
--- js/common.js
+++ js/common.js
@@ -32,3 +32,17 @@ escapeHTML = function(string)
         return entityMap[s];
     });
 }
+
+/*
+ * Helper function:
+ * Parse all specified jQuery element's attributes into a dictionary
+ */
+jParse = function(jElement, attributes)
+{
+    var result = {};
+    for (var i=0; i<attributes.length; i++)
+    {
+        result[attributes[i]] = jElement.attr(attributes[i]); 
+    }
+    return result;
+}
